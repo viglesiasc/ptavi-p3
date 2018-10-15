ac1711361ca5427faf0a5220fe32f0184a382b88
Victor <victoriglcue@gmail.com>
2018-10-15 18:03:38 +0200

    Creación de lista de etiquetas

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -1,4 +1,40 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+from xml.sax import make_parser
+from xml.sax.handler import ContentHandler
 
+class SmallSMILHandler(ContentHandler):
+    def __init__(self):
+        self.etiqueta =[]
+
+    def startElement(self, name, attrs):
+        etiqueta = {'root-layout': {'width', 'height', 'background-color'},
+                    'region': {'id', 'top', 'bottom', 'left', 'right'},
+                    'img': {'src', 'region', 'begin', 'dur'},
+                    'audio': {'src', 'begin', 'dur'},
+                    'textstream': {'src', 'region'}}
+
+
+        if name in etiqueta:
+            variables = {}
+            for atribute in etiqueta[name]:
+                variables[atribute] = attrs.get(atribute, "")
+
+            self.etiqueta.append([name, variables])
+
+
+    def get_tags(self):
+        return self.etiqueta
+
+
+
+if __name__ == "__main__":
+    """
+    Programa principal
+    """
+    parser = make_parser()
+    cHandler = SmallSMILHandler()
+    parser.setContentHandler(cHandler)
+    parser.parse(open('karaoke.smil'))
+    print(cHandler.get_tags())
