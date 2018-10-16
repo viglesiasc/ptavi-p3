e4c41307d933b1b571b99eb46e4dd69946be9ddf
Victor <victoriglcue@gmail.com>
2018-10-16 18:24:12 +0200

    Modificado para crear fichero JSON

--- karaoke.py
+++ karaoke.py
@@ -5,6 +5,7 @@ from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 from smallsmilhandler import SmallSMILHandler
 
+import json
 import sys
 if __name__ == "__main__":
     """
@@ -20,15 +21,23 @@ if __name__ == "__main__":
 
         etiquetas = cHandler.get_tags()
 
+        nombre_json = fichero[:-4] + 'json'
+        fichero_json = open(nombre_json, 'w')
         for linea in etiquetas:
             elemento = linea[0]
             atributos = linea[1]
             print(str(elemento), end='')
+            json.dump(elemento, fichero_json)
             for atributo in atributos:
                 if atributos[atributo] != '':
+                    json.dump("\t" + atributo + "="
+                              + (atributos[atributo]), fichero_json)
+
                     print("\\t" + str(atributo) + "=" + '"'
                           + str(atributos[atributo])
                           + '"', end='')
+            json.dump('\n', fichero_json)
             print('\\n')
+
     except IndexError:
         sys.exit("Usage: python3 karaoke.py file.smil")
