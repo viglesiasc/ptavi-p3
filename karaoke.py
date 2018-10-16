c4bb096777154c58ec88686ea7a4e381cf9b6632
Victor <victoriglcue@gmail.com>
2018-10-16 17:28:02 +0200

    Ejercicio 4 sin fichero JSON

---
+++ karaoke.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from xml.sax import make_parser
+from xml.sax.handler import ContentHandler
+from smallsmilhandler import SmallSMILHandler
+
+import sys
+
+
+
+
+if __name__ == "__main__":
+    """
+    Programa principal
+    """
+    try:
+        fichero = sys.argv[1]
+
+        parser = make_parser()
+        cHandler = SmallSMILHandler()
+        parser.setContentHandler(cHandler)
+        parser.parse(open(fichero))
+
+        etiquetas = cHandler.get_tags()
+
+        for linea in etiquetas:
+            elemento = linea[0]
+            atributos = linea[1]
+            print(str(elemento) , end='')
+            for atributo in atributos:
+                if atributos[atributo] != '':
+                    print("\\t" + str(atributo) + "=" + '"' + str(atributos[atributo]) + '"', end='')
+            print('\\n')
+    except IndexError:
+        sys.exit("Usage: python3 karaoke.py file.smil")
