c31aed07443d7105ee1bdca16d5e640f35f37e4d
Victor <victoriglcue@gmail.com>
2018-10-16 17:36:01 +0200

    Pasado PEP8

--- karaoke.py
+++ karaoke.py
@@ -6,10 +6,6 @@ from xml.sax.handler import ContentHandler
 from smallsmilhandler import SmallSMILHandler
 
 import sys
-
-
-
-
 if __name__ == "__main__":
     """
     Programa principal
@@ -27,10 +23,12 @@ if __name__ == "__main__":
         for linea in etiquetas:
             elemento = linea[0]
             atributos = linea[1]
-            print(str(elemento) , end='')
+            print(str(elemento), end='')
             for atributo in atributos:
                 if atributos[atributo] != '':
-                    print("\\t" + str(atributo) + "=" + '"' + str(atributos[atributo]) + '"', end='')
+                    print("\\t" + str(atributo) + "=" + '"'
+                          + str(atributos[atributo])
+                          + '"', end='')
             print('\\n')
     except IndexError:
         sys.exit("Usage: python3 karaoke.py file.smil")
